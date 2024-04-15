49287742244e76fb0d140ee8a3e20e7c49cb4b84
Artem Savelev <a.savelev@profsoft.pro>
2024-04-15 17:42:20 +0700

    added request test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from "axios";
 
-function App() {
+const App = () => {
+
+  const [text, setText] = useState<string>('')
+
+  useEffect(() => {
+    axios.get('http://localhost:4000').then((resp) => {
+      console.log('resp', resp)
+      setText(resp.data)
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <>
+        {text}
+      </>
+  )
 }
 
 export default App;
